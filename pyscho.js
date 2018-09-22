875a7bfa4f6c8295d00440e648af44dd5c1a4d51
kheengz <kheengz@gmail.com>
2018-09-22 10:47:11 +0100

    reOrder any level of nested array algorithm

---
+++ pyscho.js
@@ -0,0 +1,56 @@
+
+physcoMetrics = (scores, lowerLimits, upperLimits) => {
+    const result = [];
+    const ranges = lowerLimits.map((lower, index) => {
+        return [lower, upperLimits[index]];
+    });
+
+    function sort(array) {
+        array.sort(function (a, b) {
+            return a - b
+        })
+    }
+
+    sort(scores);
+    const min = Math.min(...scores);
+    const max = Math.max(...scores);
+    console.log('scores : ', scores);
+    for (let i = 0; i < ranges.length; i++) {
+        const bound = ranges[i];
+        if(bound[0] > min){
+
+        }
+        let minIndex = -1;
+        let maxIndex = -1;
+        let count = 0;
+        console.log('bound : ', bound);
+        if (bound[0] <= min) {
+            minIndex = scores.indexOf(min);
+            console.log('minIndex : ', minIndex);
+        }
+        if (bound[1] >= max) {
+            maxIndex = scores.indexOf(max);
+            console.log('maxIndex : ', maxIndex);
+        }
+        if (minIndex >= 0 && maxIndex >= 0) {
+            console.log('minIndex, maxIndex : ', minIndex, maxIndex);
+            count = (maxIndex - minIndex) + 1;
+            console.log('count : ', count);
+        }
+        console.log('max, min : ', max, min);
+        result.push(count);
+        break;
+    }
+    // result.push(count);
+    return result;
+};
+
+// const scores = [1, 3, 5, 6, 8];
+// const lower = [2];
+// const upper = [6];
+
+const scores = [4, 8, 7];
+const lower = [2, 4];
+const upper = [8, 4];
+
+console.log("pysco : ", physcoMetrics(scores, lower, upper));

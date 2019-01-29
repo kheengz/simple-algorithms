b82f745ea1c54404b83b24e5ae00dad255197f51
kheengz <kheengz@gmail.com>
2019-01-29 15:20:57 +0100

    created is palindrome and prefix of string algorithms

---
+++ tomb.js
@@ -0,0 +1,46 @@
+
+tomb = (A) => {
+    const basePoint = {x: 0, y: 0};
+    function getSlope(a, b) {
+        let up = b.y - a.y;
+        let down = b.x - a.x;
+        return up / down;
+    }
+    function isInSameQuadrant(x1, x2){
+        return (( (x1.x > 0 &&  x2.x > 0) ||  (x1.x < 0 &&  x2.x < 0))
+            && ( (x1.y > 0 && x2.y > 0) || (x1.y < 0 && x2.y < 0)));
+    }
+    for (let i = 0; i < A.length; i++) {
+        const baseSlope = getSlope(basePoint, A[i]);
+        for (let j = (i + 1); j < A.length; j++) {
+            const currentSlope = getSlope(A[j], A[i]);
+            if(isInSameQuadrant(A[j], A[i]) && baseSlope === currentSlope){
+                A.splice(j, 1);
+            }
+        }
+    }
+    return A.length;
+};
+
+// const points = [
+//     {x: -1, y: -2},
+//     {x: 1, y: 2},
+//     {x: 2, y: 4},
+//     {x: -3, y: 2},
+//     {x: 2, y: -2},
+// ];
+
+const points = [
+    {x: -4, y: 2},
+    {x: 1, y: 3},
+    {x: 3, y: 3},
+    {x: 5, y: 5},
+    {x: 4, y: 1},
+    {x: 2, y: 2},
+    {x: 3, y: -6},
+    {x: -6, y: 3},
+    {x: 2, y: -2},
+    {x: 2, y: -4},
+];
+console.log('points : ', points);
+console.log("result : ", tomb(points));
